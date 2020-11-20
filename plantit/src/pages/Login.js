16832116251635e3463a3b02f5173f39131d82dd
piupaulgit <contactpiu619@gmail.com>
2020-11-20 22:17:20 +0530

    responsive login screen

--- plantit/src/pages/Login.js
+++ plantit/src/pages/Login.js
@@ -7,13 +7,13 @@ const Login = () => {
         <div className="login">
             <Container fluid className="p-0">
                 <Row className="m-0">
-                    <Col md={6} className="login__left bg-pink d-flex align-item-between">
+                    <Col md={5} lg={6} className="login__left bg-pink d-flex align-item-between">
                         <div>
                             <span className="logo logo--large"></span>
                             <h1 className="text-uppercase font-bold">Plan your project <br></br>like a PRO</h1>
                         </div>
                     </Col>
-                    <Col md={6} className="login__right">
+                    <Col md={7} lg={6} className="login__right">
                         <Form>
                             <Form.Group controlId="email">
                                 <Form.Label>Email address</Form.Label>
