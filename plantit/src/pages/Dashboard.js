421d24fd052822eecf561aa7e07e1d856f27ed97
piupaulgit <contactpiu619@gmail.com>
2021-01-05 23:51:43 +0530

    signout btn added

--- plantit/src/pages/Dashboard.js
+++ plantit/src/pages/Dashboard.js
@@ -4,6 +4,7 @@ function Dashboard() {
     return (
         <div className="dashboard">
             <h1>I am from dashboard</h1>
+            <button className="btn btn-primary">Sign out</button>
         </div>
     )
 }
